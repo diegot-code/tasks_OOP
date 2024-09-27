5e356ab549bc8782211ed5d0ab576446f3fb65f5
diegot-code <torres.db04@gmail.com>
2024-09-27 18:42:16 -0400

    Creation Feature works

--- process.php
+++ process.php
@@ -1,13 +1,15 @@
 <?php
 
 session_start();
-
+require_once("dbc.php");
 
 $title = $_POST['title'];
 $desc = $_POST['description'];
 
 $sql = "INSERT INTO tasks (task_title, task_description, user_id)
 VALUES ('$desc', '$title', '1')";
+
+echo $sql;die;
 
 if (mysqli_query($conn, $sql)) {
   header("Location: dashboard.php");
