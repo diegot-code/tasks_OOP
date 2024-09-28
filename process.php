f5607894525697a640a45366a8a65beca6688ef1
diegot-code <torres.db04@gmail.com>
2024-09-27 23:18:41 -0400

    Changed indiv if stmts to switch case

--- process.php
+++ process.php
@@ -3,11 +3,17 @@
 session_start();
 require_once("dbc.php");
 
+if($_SERVER['REQUEST_METHOD'] != "POST") {
+    header("Location: dashboard.php");
+}
+
 
 class Task {
     public $task_id;
     public $task_title;
     public $task_desc;
+// give example
+    public $table_name = "tasks";
 
     private $conn;
 
@@ -18,58 +24,95 @@ class Task {
         header("Location: index.php");
     }
     }
+    
+    public function addTask() {
+        $query = "INSERT INTO $this->$table_name (task_title, task_description, user_id)
+    VALUES ('$this->task_title', '$this->task_desc', '1')";
 
-
-
-
-}
-
-if(isset($_POST['addTask'])) {
-    $title = $_POST['title'];
-    $desc = $_POST['description'];
-
-    $addsql = "INSERT INTO tasks (task_title, task_description, user_id)
-    VALUES ('$title', '$desc', '1')";
-
-    if (mysqli_query($conn, $addsql)) {
-    header("Location: dashboard.php");
-    } else {
-    echo "Error: " . $addsql . "<br>" . mysqli_error($conn);
-    }
-
-    mysqli_close($conn);
-}
-
-if(isset($_POST['updateTask'])) {
-    $id = $_POST['taskId'];
-    $title = $_POST['title'];
-    $desc = $_POST['description'];
-
-    $updatesql = "UPDATE tasks SET task_title='$title', task_description='$desc' WHERE task_id=$id";
-    if (mysqli_query($conn, $updatesql)) {
-     header("Location: dashboard.php");
-    } else {
-    echo "Error updating record: " . mysqli_error($conn);
-    }
-
-    mysqli_close($conn);
-}
-
-if(isset($_POST['deleteTask'])) {
-    $id = $_POST['taskId'];
-
-    // sql to delete a record
-    $deletesql = "DELETE FROM tasks WHERE task_id=$id";
-
-    if (mysqli_query($conn, $deletesql)) {
+    if (mysqli_query($this->conn, $query)) {
         header("Location: dashboard.php");
     } else {
-    echo "Error deleting record: " . mysqli_error($conn);
+        echo "Error: " . $addsql . "<br>" . mysqli_error($conn);
+    }
     }
 
-    mysqli_close($conn);
 }
 
+
+// if(isset($_POST['updateTask'])) {
+//     $id = $_POST['taskId'];
+//     $title = $_POST['title'];
+//     $desc = $_POST['description'];
+
+//     $updatesql = "UPDATE tasks SET task_title='$title', task_description='$desc' WHERE task_id=$id";
+//     if (mysqli_query($conn, $updatesql)) {
+//      header("Location: dashboard.php");
+//     } else {
+//     echo "Error updating record: " . mysqli_error($conn);
+//     }
+
+//     mysqli_close($conn);
+// }
+
+// if(isset($_POST['deleteTask'])) {
+//     $id = $_POST['taskId'];
+
+//     // sql to delete a record
+//     $deletesql = "DELETE FROM tasks WHERE task_id=$id";
+
+//     if (mysqli_query($conn, $deletesql)) {
+//         header("Location: dashboard.php");
+//     } else {
+//     echo "Error deleting record: " . mysqli_error($conn);
+//     }
+
+//     mysqli_close($conn);
+// }
+
+
+switch($_POST['processType']) {
+    case 'addTask':
+        $title = $_POST['title'];
+        $desc = $_POST['description'];
+
+        $addsql = "INSERT INTO tasks (task_title, task_description, user_id)
+        VALUES ('$title', '$desc', '1')";
+
+        if (mysqli_query($conn, $addsql)) {
+        header("Location: dashboard.php");
+        } else {
+        echo "Error: " . $addsql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+    case 'updateTask':
+        $id = $_POST['taskId'];
+        $title = $_POST['title'];
+        $desc = $_POST['description'];
+
+        $updatesql = "UPDATE tasks SET task_title='$title', task_description='$desc' WHERE task_id=$id";
+        if (mysqli_query($conn, $updatesql)) {
+        header("Location: dashboard.php");
+        } else {
+        echo "Error updating record: " . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+    case 'deleteTask':
+        $id = $_POST['taskId'];
+
+        // sql to delete a record
+        $deletesql = "DELETE FROM tasks WHERE task_id=$id";
+
+        if (mysqli_query($conn, $deletesql)) {
+            header("Location: dashboard.php");
+        } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+
+}
 
 
 ?>
