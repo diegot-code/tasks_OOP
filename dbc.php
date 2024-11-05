6467f34c29672c67eeeee375aadeb71c56105bc7
diegot-code <torres.db04@gmail.com>
2024-11-04 19:09:13 -0500

    Changed Connection to OOP

--- dbc.php
+++ dbc.php
@@ -6,8 +6,8 @@ $password = "root";
 $dbname = "task-manager";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
