2cacd2afd3b43dc1cb85b5a5a7c2d8813631eb20
diegot-code <torres.db04@gmail.com>
2024-11-05 17:31:53 -0500

    Deleted Unecessary Comments

--- process.php
+++ process.php
@@ -92,69 +92,11 @@ class Task {
 
 }
 
-// class User {
-//     public $UID;
-//     public $username;
-//     public $email;
-
-//     public function whoAmI() {
-//         echo $this->UID . " " . $this->username . " " . $this->email;
-//     }
-
-// }
-
-// if(isset($_POST['updateTask'])) {
-//     $id = $_POST['taskId'];
-//     $title = $_POST['title'];
-//     $desc = $_POST['description'];
-
-//     $updatesql = "UPDATE tasks SET task_title='$title', task_description='$desc' WHERE task_id=$id";
-//     if (mysqli_query($conn, $updatesql)) {
-//      header("Location: dashboard.php");
-//     } else {
-//     echo "Error updating record: " . mysqli_error($conn);
-//     }
-
-//     mysqli_close($conn);
-// }
-
-// if(isset($_POST['deleteTask'])) {
-//     $id = $_POST['taskId'];
-
-//     // sql to delete a record
-//     $deletesql = "DELETE FROM tasks WHERE task_id=$id";
-
-//     if (mysqli_query($conn, $deletesql)) {
-//         header("Location: dashboard.php");
-//     } else {
-//     echo "Error deleting record: " . mysqli_error($conn);
-//     }
-
-//     mysqli_close($conn);
-// }
-
-
-// echo $_POST['processType'];die;
 
 switch($_POST['processType']) {
     case 'addTask':
         // MARK: Add Task
-        // $title = $_POST['title'];
-        // $desc = $_POST['description'];
 
-        // $addsql = "INSERT INTO tasks (task_title, task_description, user_id)
-        // VALUES ('$title', '$desc', '1')";
-
-        // if ($conn->query($addsql) === TRUE) {
-        //     header("Location: dashboard.php");
-        //     exit;
-        // } else {
-        //     echo "Error: " . $addsql . "<br>" . $conn->error;
-        // }
-
-        // mysqli_close($conn);
-        
-        
         // Initialising a Task class
         $task = new Task($servername, $username, $password, $dbname);
         
@@ -177,19 +119,6 @@ switch($_POST['processType']) {
     case 'updateTask':
         // MARK: Update Task
 
-        // $id = $_POST['taskId'];
-        // $title = $_POST['title'];
-        // $desc = $_POST['description'];
-
-        // $updatesql = "UPDATE tasks SET task_title='$title', task_description='$desc' WHERE task_id=$id";
-        // if ($conn->query($updatesql) === TRUE) {
-        // header("Location: dashboard.php");
-        // exit;
-        // } else {
-        // echo "Error updating record: " . $conn->error;
-        // }
-
-        // mysqli_close($conn);
         $task = new Task($servername, $username, $password, $dbname);
         
         // Set the title and Desc
@@ -208,19 +137,6 @@ switch($_POST['processType']) {
     case 'deleteTask':
         // MARK: Delete Task
 
-        // $id = $_POST['taskId'];
-
-        // // sql to delete a record
-        // $deletesql = "DELETE FROM tasks WHERE task_id=$id";
-
-        // if ($conn->query($deletesql) === TRUE) {
-        //     header("Location: dashboard.php");
-        //     exit;
-        // } else {
-        // echo "Error deleting record: " . $conn->error;
-        // }
-
-        // mysqli_close($conn);
         $task = new Task($servername, $username, $password, $dbname);
         
         $task->tid = $_POST['taskId'];
@@ -237,9 +153,8 @@ switch($_POST['processType']) {
         $id = $_POST['user_id'];
 
         $findUserQuery = "SELECT * FROM users WHERE user_id=$id";
-        // echo $findUserQuery;die;
+
         $response = $conn->query($findUserQuery);
-        // echo mysqli_num_rows($response);die;
 
         if ($response->num_rows > 0) {
             $_SESSION['userLoggedID'] = $id;
